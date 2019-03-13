0aeb160f1b33ec1279dd5f228c6ef94e01fa410c
Abr Studio <abr@ABRs-iMac.local>
2019-03-13 13:12:39 +0330

    Initial Commit

---
+++ MetrixSdk/MetrixSdk.h
@@ -0,0 +1,19 @@
+//
+//  MetrixSdk.h
+//  MetrixSdk
+//
+//  Created by Abr Studio on 3/13/19.
+//  Copyright © 2019 sdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MetrixSdk.
+FOUNDATION_EXPORT double MetrixSdkVersionNumber;
+
+//! Project version string for MetrixSdk.
+FOUNDATION_EXPORT const unsigned char MetrixSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetrixSdk/PublicHeader.h>
+
+
